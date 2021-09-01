a4d01acc09b42490d69902f7be96818aaf408d38
Carlos Asmat <casmat@dephy.com>
2021-09-01 11:21:15 -0400

    automatic fixes to files

--- Part_2/Python/calculateGroundSlope/testCalculateGroundSlopeFunction.py
+++ Part_2/Python/calculateGroundSlope/testCalculateGroundSlopeFunction.py
@@ -2,6 +2,7 @@
 import calculateGroundSlope as cgs
 import matplotlib.pyplot as mplt
 import os, sys
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -9,23 +10,25 @@ import helperFunctions as hf
 
 # Test your code by running the following
 
+
 def testCalculateGroundSlope():
-    files = os.listdir('dataset')
+	files = os.listdir("dataset")
 
-    i = 0
-    for filename in files:
-        data_array = hf.load_csv_file('dataset/' + filename)
-        
-        groundSlopePredictions = cgs.calculateGroundSlope(data_array)
-        
-        mplt.figure(i+1)
-        mplt.plot(data_array[:,hf.variable_name_to_column_index('incline')], 'b')
-        mplt.plot(groundSlopePredictions,'r')
-        mplt.draw()
+	i = 0
+	for filename in files:
+		data_array = hf.load_csv_file("dataset/" + filename)
 
-        i += 1
+		groundSlopePredictions = cgs.calculateGroundSlope(data_array)
 
-    mplt.show()
+		mplt.figure(i + 1)
+		mplt.plot(data_array[:, hf.variable_name_to_column_index("incline")], "b")
+		mplt.plot(groundSlopePredictions, "r")
+		mplt.draw()
+
+		i += 1
+
+	mplt.show()
+
 
 if __name__ == "__main__":
-    testCalculateGroundSlope()
+	testCalculateGroundSlope()
--- Part_2/Python/detectWalking/testDetectWalkingFunction.py
+++ Part_2/Python/detectWalking/testDetectWalkingFunction.py
@@ -2,6 +2,7 @@
 import detectWalking as dw
 import matplotlib.pyplot as mplt
 import os, sys
+
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -9,23 +10,25 @@ import helperFunctions as hf
 
 # Test your code by running the following
 
+
 def testDetectWalking():
-	files = os.listdir('dataset')
+	files = os.listdir("dataset")
 
 	i = 0
 	for filename in files:
-		data_array = hf.load_csv_file('dataset/' + filename)
-		
+		data_array = hf.load_csv_file("dataset/" + filename)
+
 		detectWalkingPredictions = dw.detectWalking(data_array)
-		
-		mplt.figure(i+1)
-		mplt.plot(data_array[:,hf.variable_name_to_column_index('movement')], 'b')
-		mplt.plot(detectWalkingPredictions,'r')
+
+		mplt.figure(i + 1)
+		mplt.plot(data_array[:, hf.variable_name_to_column_index("movement")], "b")
+		mplt.plot(detectWalkingPredictions, "r")
 		mplt.draw()
 
 		i += 1
 
 	mplt.show()
+
 
 if __name__ == "__main__":
 	testDetectWalking()
